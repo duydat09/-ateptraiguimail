47fa64227d59acf730182341368c322f2bbc4b36
datgreen <duydat75@gmail.com>
2018-07-19 22:46:01 +0700

    .

--- app.py
+++ app.py
@@ -11,6 +11,18 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
+    all_lover = Lover.objects()
+    now = datetime.now()
+    for item in all_lover:
+        if (now.month - item.date.month) == 0:
+            if (now.day - item.date.day) == -1:
+                email = item.user_id.email
+                name = item.fullname
+                gmail=GMail('nguyenduydat1027@gmail.com','dat12345678')
+                html = "<p>Ng&agrave;y mai l&agrave; sinh nhật của&nbsp{{name}}</p>"
+                html_content = html.replace("{{name}}", name)
+                msg=Message('Nhắc nhở sinh nhật của người yêu',to=email, html=html_content)
+                gmail.send(msg)
     
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour=6)
